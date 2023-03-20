d1c56b311ad6568843172f9bfef6efecf40d324b
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-20 14:37:00 -0300

    Se modifican la forma de escribir los innerTXT

--- js/tablasdecargos.js
+++ js/tablasdecargos.js
@@ -9,7 +9,107 @@ function verCargos(){
     const divGrafica = document.createElement("div");
     divGrafica.id = "containerGrafica"
     divGrafica.className = "containerGrafica";
-    divGrafica.innerHTML = "<div class='as1 asambleista'><h2>1</h2></div><div class='as2 asambleista'><h2>2</h2></div><div class='as3 asambleista'><h2>3</h2></div><div class='as4 asambleista'><h2>4</h2></div><div class='as5 asambleista'><h2>5</h2></div><div class='as6 asambleista'><h2>6</h2></div><div class='as7 asambleista'><h2>7</h2></div><div class='as8 asambleista'><h2>8</h2></div><div class='as9 asambleista'><h2>9</h2></div><div class='as10 asambleista'><h2>10</h2></div><div class='as11 asambleista'><h2>11</h2></div><div class='as12 asambleista'><h2>12</h2></div><div class='as13 asambleista'><h2>13</h2></div><div class='as14 asambleista'><h2>14</h2></div><div class='as15 asambleista'><h2>15</h2></div><div class='as16 asambleista'><h2>16</h2></div><div class='as17 asambleista'><h2>17</h2></div><div class='as18 asambleista'><h2>18</h2></div><div class='as19 asambleista'><h2>19</h2></div><div class='as20 asambleista'><h2>20</h2></div><div class='as21 asambleista'><h2>21</h2></div><div class='as22 asambleista'><h2>22</h2></div><div class='as23 asambleista'><h2>23</h2></div><div class='as24 asambleista'><h2>24</h2></div><div class='as25 asambleista'><h2>25</h2></div><div class='as26 asambleista'><h2>26</h2></div><div class='as27 asambleista'><h2>27</h2></div><div class='as28 asambleista'><h2>28</h2></div><div class='as29 asambleista'><h2>29</h2></div><div class='as30 asambleista'><h2>30</h2></div><div class='as31 asambleista'><h2>31</h2></div><div class='as32 asambleista'><h2>32</h2></div><div class='as33 asambleista'><h2>33</h2></div><div class='as34 asambleista'><h2>34</h2></div><div class='as35 asambleista'><h2>35</h2></div><div class='as36 asambleista'><h2>36</h2></div><div class='as37 asambleista'><h2>37</h2></div><div class='as38 asambleista'><h2>38</h2></div><div class='as39 asambleista'><h2>39</h2></div><div class='as40 asambleista'><h2>40</h2></div><div class='as41 asambleista'><h2>41</h2></div><div class='as42 asambleista'><h2>42</h2></div><div class='as43 asambleista'><h2>43</h2></div><div class='as44 asambleista'><h2>44</h2></div><div class='as45 asambleista'><h2>45</h2></div><div class='as46 asambleista'><h2>46</h2></div><div class='as47 asambleista'><h2>47</h2></div><div class='as48 asambleista'><h2>48</h2></div><div class='as49 asambleista'><h2>49</h2></div><div class='as50 asambleista'><h2>50</h2></div><div class='as51 asambleista'><h2>51</h2></div><div class='as52 asambleista'><h2>52</h2></div><div class='as53 asambleista'><h2>53</h2></div><div class='as54 asambleista'><h2>54</h2></div><div class='as55 asambleista'><h2>55</h2></div><div class='as56 asambleista'><h2>56</h2></div><div class='as57 asambleista'><h2>57</h2></div><div class='as58 asambleista'><h2>58</h2></div><div class='as59 asambleista'><h2>59</h2></div><div class='as60 asambleista'><h2>60</h2></div><div class='as61 asambleista'><h2>61</h2></div><div class='as62 asambleista'><h2>62</h2></div><div class='as63 asambleista'><h2>63</h2></div><div class='as64 asambleista'><h2>64</h2></div><div class='as65 asambleista'><h2>65</h2></div><div class='as66 asambleista'><h2>66</h2></div><div class='as67 asambleista'><h2>67</h2></div><div class='as68 asambleista'><h2>68</h2></div><div class='as69 asambleista'><h2>69</h2></div><div class='as70 asambleista'><h2>70</h2></div><div class='as71 asambleista'><h2>71</h2></div><div class='as72 asambleista'><h2>72</h2></div><div class='as73 asambleista'><h2>73</h2></div><div class='as74 asambleista'><h2>74</h2></div><div class='as75 asambleista'><h2>75</h2></div><div class='as76 asambleista'><h2>76</h2></div><div class='as77 asambleista'><h2>77</h2></div><div class='as78 asambleista'><h2>78</h2></div><div class='as79 asambleista'><h2>79</h2></div><div class='as80 asambleista'><h2>80</h2></div><div class='as81 asambleista'><h2>81</h2></div><div class='as82 asambleista'><h2>82</h2></div><div class='as83 asambleista'><h2>83</h2></div><div class='as84 asambleista'><h2>84</h2></div><div class='as85 asambleista'><h2>85</h2></div><div class='as86 asambleista'><h2>86</h2></div><div class='as87 asambleista'><h2>87</h2></div><div class='as88 asambleista'><h2>88</h2></div><div class='as89 asambleista'><h2>89</h2></div><div class='as90 asambleista'><h2>90</h2></div>";
+    divGrafica.innerHTML = `<div class='as1 asambleista'>
+                                <h2>1</h2>
+                            </div>
+                            <div class='as2 asambleista'>
+                                <h2>2</h2>
+                            </div>
+                            <div class='as3 asambleista'>
+                                <h2>3</h2>
+                            </div>
+                            <div class='as4 asambleista'>
+                                <h2>4</h2>
+                            </div>
+                            <div class='as5 asambleista'>
+                                <h2>5</h2>
+                            </div>
+                            
+                            <div class='as6 asambleista'><h2>6</h2></div>
+                            <div class='as7 asambleista'><h2>7</h2></div>
+                            <div class='as8 asambleista'><h2>8</h2></div>
+                            <div class='as9 asambleista'><h2>9</h2></div>
+                            <div class='as10 asambleista'><h2>10</h2></div>
+                            <div class='as11 asambleista'><h2>11</h2></div>
+                            <div class='as12 asambleista'><h2>12</h2></div>
+                            <div class='as13 asambleista'><h2>13</h2></div>
+                            <div class='as14 asambleista'><h2>14</h2></div>
+                            <div class='as15 asambleista'><h2>15</h2></div>
+                            <div class='as16 asambleista'><h2>16</h2></div>
+                            <div class='as17 asambleista'><h2>17</h2></div>
+                            <div class='as18 asambleista'><h2>18</h2></div>
+                            <div class='as19 asambleista'><h2>19</h2></div>
+                            <div class='as20 asambleista'><h2>20</h2></div>
+                            <div class='as21 asambleista'><h2>21</h2></div>
+                            <div class='as22 asambleista'><h2>22</h2></div>
+                            <div class='as23 asambleista'><h2>23</h2></div>
+                            <div class='as24 asambleista'><h2>24</h2></div>
+                            <div class='as25 asambleista'><h2>25</h2></div>
+                            <div class='as26 asambleista'><h2>26</h2></div>
+                            <div class='as27 asambleista'><h2>27</h2></div>
+                            <div class='as28 asambleista'><h2>28</h2></div>
+                            <div class='as29 asambleista'><h2>29</h2></div>
+                            <div class='as30 asambleista'><h2>30</h2></div>
+                            <div class='as31 asambleista'><h2>31</h2></div>
+                            <div class='as32 asambleista'><h2>32</h2></div>
+                            <div class='as33 asambleista'><h2>33</h2></div>
+                            <div class='as34 asambleista'><h2>34</h2></div>
+                            <div class='as35 asambleista'><h2>35</h2></div>
+                            <div class='as36 asambleista'><h2>36</h2></div>
+                            <div class='as37 asambleista'><h2>37</h2></div>
+                            <div class='as38 asambleista'><h2>38</h2></div>
+                            <div class='as39 asambleista'><h2>39</h2></div>
+                            <div class='as40 asambleista'><h2>40</h2></div>
+                            <div class='as41 asambleista'><h2>41</h2></div>
+                            <div class='as42 asambleista'><h2>42</h2></div>
+                            <div class='as43 asambleista'><h2>43</h2></div>
+                            <div class='as44 asambleista'><h2>44</h2></div>
+                            <div class='as45 asambleista'><h2>45</h2></div>
+                            <div class='as46 asambleista'><h2>46</h2></div>
+                            <div class='as47 asambleista'><h2>47</h2></div>
+                            <div class='as48 asambleista'><h2>48</h2></div>
+                            <div class='as49 asambleista'><h2>49</h2></div>
+                            <div class='as50 asambleista'><h2>50</h2></div>
+                            <div class='as51 asambleista'><h2>51</h2></div>
+                            <div class='as52 asambleista'><h2>52</h2></div>
+                            <div class='as53 asambleista'><h2>53</h2></div>
+                            <div class='as54 asambleista'><h2>54</h2></div>
+                            <div class='as55 asambleista'><h2>55</h2></div>
+                            <div class='as56 asambleista'><h2>56</h2></div>
+                            <div class='as57 asambleista'><h2>57</h2></div>
+                            <div class='as58 asambleista'><h2>58</h2></div>
+                            <div class='as59 asambleista'><h2>59</h2></div>
+                            <div class='as60 asambleista'><h2>60</h2></div>
+                            <div class='as61 asambleista'><h2>61</h2></div>
+                            <div class='as62 asambleista'><h2>62</h2></div>
+                            <div class='as63 asambleista'><h2>63</h2></div>
+                            <div class='as64 asambleista'><h2>64</h2></div>
+                            <div class='as65 asambleista'><h2>65</h2></div>
+                            <div class='as66 asambleista'><h2>66</h2></div>
+                            <div class='as67 asambleista'><h2>67</h2></div>
+                            <div class='as68 asambleista'><h2>68</h2></div>
+                            <div class='as69 asambleista'><h2>69</h2></div>
+                            <div class='as70 asambleista'><h2>70</h2></div>
+                            <div class='as71 asambleista'><h2>71</h2></div>
+                            <div class='as72 asambleista'><h2>72</h2></div>
+                            <div class='as73 asambleista'><h2>73</h2></div>
+                            <div class='as74 asambleista'><h2>74</h2></div>
+                            <div class='as75 asambleista'><h2>75</h2></div>
+                            <div class='as76 asambleista'><h2>76</h2></div>
+                            <div class='as77 asambleista'><h2>77</h2></div>
+                            <div class='as78 asambleista'><h2>78</h2></div>
+                            <div class='as79 asambleista'><h2>79</h2></div>
+                            <div class='as80 asambleista'><h2>80</h2></div>
+                            <div class='as81 asambleista'><h2>81</h2></div>
+                            <div class='as82 asambleista'><h2>82</h2></div>
+                            <div class='as83 asambleista'><h2>83</h2></div>
+                            <div class='as84 asambleista'><h2>84</h2></div>
+                            <div class='as85 asambleista'><h2>85</h2></div>
+                            <div class='as86 asambleista'><h2>86</h2></div>
+                            <div class='as87 asambleista'><h2>87</h2></div>
+                            <div class='as88 asambleista'><h2>88</h2></div>
+                            <div class='as89 asambleista'><h2>89</h2></div>
+                            <div class='as90 asambleista'><h2>90</h2></div>`;
     pie.appendChild(divGrafica);
     
     //Crea título COMISIÓN DIRECTIVA
@@ -22,7 +122,66 @@ function verCargos(){
     const divCD = document.createElement("div");
     divCD.id = "containerCD";
     divCD.className = "containerCD";
-    divCD.innerHTML = "<div class='cd1 directivo'><h2>1</h2></div><div class='cd2 directivo'><h2>2</h2></div><div class='cd3 directivo'><h2>3</h2></div><div class='cd4 directivo'><h2>4</h2></div><div class='cd5 directivo'><h2>5</h2></div><div class='cd6 directivo'><h2>6</h2></div><div class='cd7 directivo'><h2>7</h2></div><div class='cd8 directivo'><h2>8</h2></div><div class='cd9 directivo'><h2>9</h2></div><div class='cd10 directivo'><h2>10</h2></div><div class='cd11 directivo'><h2>11</h2></div><div class='cd12 directivo'><h2>12</h2></div><div class='cd13 directivo'><h2>13</h2></div><div class='cd14 directivo'><h2>14</h2></div><div class='cd15 directivo'><h2>15</h2></div><div class='cd16 directivo'><h2>16</h2></div><div class='cd17 directivo'><h2>17</h2></div><div class='cd18 directivo'><h2>18</h2></div><div class='cd19 directivo'><h2>19</h2></div><div class='cd20 directivo'><h2>20</h2></div>";
+    divCD.innerHTML = `<div class='cd1 directivo'>
+                            <h2>1</h2>
+                        </div>
+                        <div class='cd2 directivo'>
+                            <h2>2</h2>
+                        </div>
+                        <div class='cd3 directivo'>
+                            <h2>3</h2>
+                        </div>
+                        <div class='cd4 directivo'>
+                            <h2>4</h2>
+                        </div>
+                        <div class='cd5 directivo'>
+                            <h2>5</h2>
+                        </div>
+                        <div class='cd6 directivo'>
+                            <h2>6</h2>
+                        </div>
+                        <div class='cd7 directivo'>
+                            <h2>7</h2>
+                        </div>
+                        <div class='cd8 directivo'>
+                            <h2>8</h2>
+                        </div>
+                        <div class='cd9 directivo'>
+                            <h2>9</h2>
+                        </div>
+                        <div class='cd10 directivo'>
+                            <h2>10</h2>
+                        </div>
+                        <div class='cd11 directivo'>
+                            <h2>11</h2>
+                        </div>
+                        <div class='cd12 directivo'>
+                            <h2>12</h2>
+                        </div>
+                        <div class='cd13 directivo'>
+                            <h2>13</h2>
+                        </div>
+                        <div class='cd14 directivo'>
+                            <h2>14</h2>
+                        </div>
+                        <div class='cd15 directivo'>
+                            <h2>15</h2>
+                        </div>
+                        <div class='cd16 directivo'>
+                            <h2>16</h2>
+                        </div>
+                        <div class='cd17 directivo'>
+                            <h2>17</h2>
+                        </div>
+                        <div class='cd18 directivo'>
+                            <h2>18</h2>
+                        </div>
+                        <div class='cd19 directivo'>
+                            <h2>19</h2>
+                        </div>
+                        <div class='cd20 directivo'>
+                            <h2>20</h2>
+                        </div>`;
     pie.appendChild(divCD);
     
     //Crea título COMISIÓN FISCALIZADORA
@@ -35,7 +194,27 @@ function verCargos(){
     const divCF = document.createElement("div");
     divCF.id = "divCF";
     divCF.className = "gridCF";
-    divCF.innerHTML = "<div class='cf1 fiscalizador'><h2>1</h2></div><div class='cf2 fiscalizador'><h2>2</h2></div><div class='cf3 fiscalizador'><h2>3</h2></div><div class='cf4 fiscalizador'><h2>4</h2></div><div class='cf5 fiscalizador'><h2>5</h2></div><div class='cf6 fiscalizador'><h2>6</h2></div><div class='cf7 fiscalizador'><h2>7</h2></div>";
+    divCF.innerHTML = `<div class='cf1 fiscalizador'>
+                            <h2>1</h2>
+                        </div>
+                        <div class='cf2 fiscalizador'>
+                            <h2>2</h2>
+                        </div>
+                        <div class='cf3 fiscalizador'>
+                            <h2>3</h2>
+                        </div>
+                        <div class='cf4 fiscalizador'>
+                            <h2>4</h2>
+                        </div>
+                        <div class='cf5 fiscalizador'>
+                            <h2>5</h2>
+                        </div>
+                        <div class='cf6 fiscalizador'>
+                            <h2>6</h2>
+                        </div>
+                        <div class='cf7 fiscalizador'>
+                            <h2>7</h2>
+                        </div>`;
     pie.appendChild(divCF);  
         
     //Bucle para pintar los divs según resultado de las elecciones.
