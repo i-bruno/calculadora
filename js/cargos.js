ecf22229fa4c91744ac9043ddc26b49e64365bde
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-27 15:34:13 -0300

    Se cambia el estilo

---
+++ js/cargos.js
@@ -0,0 +1,44 @@
+function verCargos(){
+ //Crea título ASAMBLEÍSTAS
+    const tituloAsambleista = document.createElement("h1");
+    tituloAsambleista.id = "tituloAsambleista";
+    tituloAsambleista.innerText = "Asambleístas";
+    pie.appendChild(tituloAsambleista);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
