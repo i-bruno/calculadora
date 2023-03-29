dfaba5a9f8fe1402a1b7bf4c3bee4bffa2e12499
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-29 11:44:30 -0300

    probando api

--- js/api.js
+++ js/api.js
@@ -1,11 +1,4 @@
-const options = {
-    method: 'GET',
-    headers: {
-        'key_archivocasla': 'b45c1bb6580d6623321f90a9e2e0c660ee8fd984'
-    }
-};
-
-fetch("https://repositorio.archivocasla.com.ar/api/items", options)
+fetch("https://repositorio.archivocasla.com.ar/api?keyapi=b45c1bb6580d6623321f90a9e2e0c660ee8fd984", options)
     .then(res => res.json())
     .then(response =>{
         console.log(response)
