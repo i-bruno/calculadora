c4e8740edf061bbc076d18a22ebf2f1b41739dd5
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-29 12:10:03 -0300

    probando

--- js/api.js
+++
@@ -1,5 +0,0 @@
-fetch("http://repositorio.archivocasla.com.ar/api?keyapi=b45c1bb6580d6623321f90a9e2e0c660ee8fd984", options)
-    .then(res => res.json())
-    .then(response =>{
-        console.log(response)
-    });
