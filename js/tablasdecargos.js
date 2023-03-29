dc687740f0a0a7afed3f9a61da80d8b0a4a7dc71
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-29 00:33:51 -0300

    probando

--- js/tablasdecargos.js
+++ js/tablasdecargos.js
@@ -433,7 +433,7 @@ function verCargos() {
 
     //Cargos para la segunda lista
 
-    for (let i = arrayListas[1].AR; i >= arrayListas[1].AR; i++) {
+    for (let i = arrayListas[0].AR; i >= arrayListas[1].AR; i++) {
         let clasesActuales = asambleista[i].getAttribute('class');
         asambleista[i].setAttribute('class', `${clasesActuales} fondo-${arrayListas[1].color}`);
     };
