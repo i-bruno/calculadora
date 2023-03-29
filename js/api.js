b692aaa0e62a5a0bd40bcb255f045d24a08ec67b
Ignacio Bruno <75222329+i-bruno@users.noreply.github.com>
2023-03-29 10:12:01 -0300

    probando

--- js/api.js
+++ js/api.js
@@ -1,2 +1,16 @@
-fetch('http://indice.archivocasla.com.ar/vocab/services.php');
-console.log(fetch('http://indice.archivocasla.com.ar/vocab/services.php'));
+const apiRepo = "http://repositorio.archivocasla.com.ar/api/items";
+const contenedorInfo = document.getElementById("contenedorInfo");
+
+fetch(apiRepo)
+    .then(respuesta => respuesta.json())
+    .then((datos) =>{
+        console.log(datos);
+    })
+
+    .catch(error => console.log(error));
+
+    function mostrarInfo (datos){
+        datos.forEach(info => {
+            const inf = document.createElement("p");
+        });
+    };
