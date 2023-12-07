ecea6483e8d05f023c7278fed570069bb98f4748
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-12-07 17:10:36 -0300

    Probando el error

--- js/tablasdecargos.js
+++ js/tablasdecargos.js
@@ -432,6 +432,7 @@ function verCargos() {
     //Cargos para la segunda lista
 
     for (let i = arrayListas[0].AR; i >= arrayListas[1].AR; i++) {
+        console.log("Index:", i, "asambleista:", asambleista[i]);
         let clasesActuales = asambleista[i].getAttribute('class');
         asambleista[i].setAttribute('class', `${clasesActuales} fondo-${arrayListas[1].color}`);
     };
