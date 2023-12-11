9b8cc8c3f4688ce3a64468c302451003ddc57e7c
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-12-11 12:45:18 -0300

    Se trata de solución para el problema del color

--- js/color.js
+++ js/color.js
@@ -2,7 +2,7 @@ const btnFondo = document.getElementById("btnColor");
 const btnEnviar = document.getElementById("btn");
 const btnResetear = document.getElementById("btnReset");
 const btnConsulta = document.getElementById("btnConsulta");
-let span1 = document.getElementById("span1");
+// const span1 = document.getElementById("span1");
 // const span2 = document.getElementById("span2");
 // const span3 = document.getElementById("span3");
 
