369e3b1d0ce06029fcc737940ab6b71e80567d34
Ignacio Bruno <ignaciobruno90@gmail.com>
2025-06-04 11:03:50 -0300

    Probando estructura de formulario cargos

--- js/cargos.js
+++ js/cargos.js
@@ -1,6 +1,6 @@
 const body = document.body;
 body.id = "cuerpo";
-body.innerHTML = '<header id="encabezado"><nav id="navBar"></nav></header> <section id="preSeccion"></section> <div id="divSeccionPrincipal"><section id="seccionPrincipal"></section><aside id="menuLateral"></aside></div> <section id="posSeccion"></section>  <section id="piePagina"></section> <footer id=footer></footer>';
+body.innerHTML = '<header id="encabezado"><nav id="navBar"></nav></header> <section id="divForm"></section> <footer id=footer></footer>';
 
 //Encabezado
 const encabezado = document.getElementById("encabezado");
@@ -10,29 +10,13 @@ encabezado.className = "classEncabezado";
 const navegacion = document.getElementById("navBar");
 navegacion.className = "classNavegacion";
 
-//Pre sección principal
-const preSeccion = document.getElementById("preSeccion");
-preSeccion.className = "classPreSeccion";
-
-//Contenedor de sección principal y info secundaria
-const divSeccionPrincipal = document.getElementById("divSeccionPrincipal");
-divSeccionPrincipal.className = "container";
+//Div formulario alta
+const divForm = document.getElementById("divForm");
+divForm.className = "divForm";
 
 ///Sección principal
 const infoPrincipal = document.getElementById("seccionPrincipal");
 infoPrincipal.className = "displayNone";
-
-///Información secundaria sobre el estatuto
-const infoSecundaria = document.getElementById("menuLateral");
-infoSecundaria.className = "gridEstatuto";
-
-//Pos sección principal
-const posSeccion = document.getElementById("posSeccion");
-posSeccion.className = "classPosSeccion";
-
-//Pie de página con los resultados
-const pie = document.getElementById("piePagina");
-pie.className = "classPie";
 
 //Último pie de página copyright
 const footer = document.getElementById("footer");
@@ -124,20 +108,29 @@ btnColor.innerText = "Rojo";
 btnColor.className = "boton";
 navegacion.appendChild(btnColor);
 
-//Tercer Nodo: ¿Cuántas agrupaciones son?
-const etiquetaPreSeccion = document.createElement("label");
-etiquetaPreSeccion.innerText = "Tipo de incidencia";
-preSeccion.appendChild(etiquetaPreSeccion);
+//Tercer Nodo: Formulario de alta
+const etiquetaIncidencia = document.createElement("label");
+etiquetaIncidencia.innerText = "Tipo de incidencia";
+divForm.appendChild(etiquetaIncidencia);
 
 ///Input para que el usuario ingrese la cantidad de listas
-const inputPreSeccion = document.createElement("input");
-inputPreSeccion.id = "numListas";
-inputPreSeccion.setAttribute('type', 'number');
-preSeccion.appendChild(inputPreSeccion);
+const selectIncidencia = document.createElement("select");
+selectIncidencia.id = "selectIncidencia";
+divForm.appendChild(selectIncidencia);
+
+const opcionIncidencia1 = document.createElement("option");
+opcionIncidencia1.innerText = "Alta";
+const opcionIncidencia2 = document.createElement("option");
+opcionIncidencia2.innerText = "Baja";
+const opcionIncidencia3 = document.createElement("option");
+opcionIncidencia3.innerText = "Licencia";
+selectIncidencia.appendChild(opcionIncidencia1);
+selectIncidencia.appendChild(opcionIncidencia2);
+selectIncidencia.appendChild(opcionIncidencia3);
 
 ///Botón para que el usuario envíe la información.
 const contenedor = document.createElement("div");
-preSeccion.appendChild(contenedor);
+divForm.appendChild(contenedor);
 
 const btn = document.createElement("button");
 btn.id = "btn";
@@ -155,112 +148,7 @@ btnReset.innerText = "Resetear";
 btnReset.addEventListener('click', _ => { location.reload(); });
 contenedor.appendChild(btnReset);
 
-//Quinto Nodo: Post sección principal
-const etiqVotosBlancos = document.createElement("label");
-etiqVotosBlancos.innerText = "Votos blancos"
-posSeccion.appendChild(etiqVotosBlancos);
 
-let votosBlancos = document.createElement("input");
-votosBlancos.setAttribute('type', 'number');
-votosBlancos.setAttribute('value', 0);
-votosBlancos.id = "votosBlancos";
-posSeccion.appendChild(votosBlancos);
-
-const etiqVotosNulos = document.createElement("label");
-etiqVotosNulos.innerText = "Votos nulos"
-posSeccion.appendChild(etiqVotosNulos);
-
-const votosNulos = document.createElement("input");
-votosNulos.setAttribute('type', 'number');
-votosNulos.setAttribute('value', 0);
-posSeccion.appendChild(votosNulos);
-
-const etiqVotosImpugnados = document.createElement("label");
-etiqVotosImpugnados.innerText = "Votos impugnados";
-posSeccion.appendChild(etiqVotosImpugnados);
-
-const votosImpugnados = document.createElement("input");
-votosImpugnados.setAttribute('type', 'number');
-votosImpugnados.setAttribute('value', 0);
-posSeccion.appendChild(votosImpugnados);
-
-const boton = document.createElement("button");
-boton.setAttribute('type', 'submit');
-boton.id = "btnConsulta";
-boton.className = "boton";
-boton.innerText = "Enviar consulta";
-boton.addEventListener("click", calculo);
-posSeccion.appendChild(boton);
-
-//Sexto Nodo: Información secundaria(aside)
-menuLateral.className = "aside";
-
-const tituloEstatuto = document.createElement("h3");
-tituloEstatuto.innerText = "Estatuto del Club Atlético San Lorenzo de Almagro";
-infoSecundaria.appendChild(tituloEstatuto);
-
-const subArt46 = document.createElement("h4");
-subArt46.innerText = "Artículo 46";
-infoSecundaria.appendChild(subArt46);
-
-const parrafoArt46 = document.createElement("p");
-parrafoArt46.innerText = "La Asamblea de representantes es la autoridad suprema de la institución. Se integra con noventa asociados y asociadas, con una integración de mujeres que asegure la representatividad de las asociadas, en proporción siempre a la composición por género del total de la masa societaria vigente al 30 de junio del año en que se celebren las elecciones, en concordancia con el objeto de la Institución, distribuidos en grupos de dos tercios para la mayoria y un tercio para la o las minorías. Los miembros correspondientes a la minoría serán elegidos según el sistema de representación proporcional por cociente. Este cociente será el resultado de dividir la suma de los votos obtenidos por las listas minoritarias por el número de miembros que corresponden a la minoría, siempre que dichos votos sean superiores al cincuenta por ciento (50%) de los votos obtenidos por la mayoría. Si las minorías obtuvieran en total menos del cincuenta por ciento (50%) de los votos obtenidos por la mayoría, ésta participará del cociente para distribuir cargos con la minoría. Obtenido el número definitivo de los representantes para la mayoría, el resto de los cargos a ocupar se distribuirá entre la o las minorías según la relación entre votos obtenidos y y el cociente. En ningún caso las minorías, en conjunto, podrán tener menos de quince (15) representantes, cualesquiera fuera el número de votos obtenidos. En este caso, la mayoría cederá las diferencias en representantes a las fracciones según el número de votos de las minorías.";
-infoSecundaria.appendChild(parrafoArt46);
-
-const subArt103 = document.createElement("h4");
-subArt103.innerText = "Artículo 103";
-infoSecundaria.appendChild(subArt103);
-
-const parrafoArt103 = document.createElement("p");
-parrafoArt103.innerText = "Realizado el cómputo final en lo que hace a los asambleístas, la Junta Electoral proclamará electos a los que ocupen los primeros puestos de la lista que hubiese obtenido el mayor número de votos hasta completar el total de sesenta (60) cargos del total de los cargos a elegir, salvo que hubiese obtenido un porcentaje mayor al cincuenta por ciento de los votos, en cuyo caso se le adjudicará el número de asambleístas que corresponda porcentualmente a esa demasía. El cupo restante -o el menor que corresponda si la lista que salió primera en los comicios obtuviera más del cincuenta por ciento de los votos escrutados, se adjudicará a las restantes listas en proporción al número de votos obtenidos por cada una, a cuyo efecto se observarán las reglas consignadas en el artículo 46.";
-infoSecundaria.appendChild(parrafoArt103);
-
-const subArt104 = document.createElement("h4");
-subArt104.innerText = "Artículo 104";
-infoSecundaria.appendChild(subArt104);
-
-const parrafoArt104 = document.createElement("p");
-parrafoArt104.innerText = "Igual procedimiento se seguirá para designar a los que habrán de integrar la Comisión Directiva, adjudicandose los quince primeros lugares del total de sus componentes a la lista que obtenga el mayor número de votos, salvo que hubiese que hubiese obtenido un porcentaje al cincuenta por ciento de los votos, en cuyo caso se adjudicará el número de miembras que corresponda a esa demasía, observándose para este caso la regla consignada en el artículo 46. Los otros cinco (5) cargos -o los que resten en su caso- se adjudicarán a las demás listas, siempre que hubieran obtenido un número superior al cuatro (4) porciento de los votos emitidos, que es el piso mínimo establecido para obtener al menos un cargo en Comisión Directiva.";
-infoSecundaria.appendChild(parrafoArt104);
-
-const subArt105 = document.createElement("h4");
-subArt105.innerText = "Artículo 105";
-infoSecundaria.appendChild(subArt105);
-
-const parrafoArt105 = document.createElement("p");
-parrafoArt105.innerText = "Si dos o mas listas empataran en la mayor cantidad de votos obtenidos para cada una, se aplicará el siguiente procedimiento:";
-infoSecundaria.appendChild(parrafoArt105);
-
-const listaArt105 = document.createElement("ol");
-infoSecundaria.appendChild(listaArt105);
-
-const primerElementoArt105 = document.createElement("li");
-primerElementoArt105.innerText = "Los dos tercios o el setenta y cinco porciento de los cargos se distribuirá entre las empatadas en partes iguales.";
-listaArt105.appendChild(primerElementoArt105);
-
-const segundoElementoArt105 = document.createElement("li");
-segundoElementoArt105.innerText = "El cincuenta por ciento de los votos obtenidos por cada una de las listas que hubieran empatado y el total de los obtenidos por las demás listas, será sumado.";
-listaArt105.appendChild(segundoElementoArt105);
-
-const tercerElementoArt105 = document.createElement("li");
-tercerElementoArt105.innerText = "El resultado de la suma anterior se dividirá por el remanente de puestos a adjudicar.";
-listaArt105.appendChild(tercerElementoArt105);
-
-const cuartoElementoArt105 = document.createElement("li");
-cuartoElementoArt105.innerText = "El cincuenta por ciento de los votos obtenidos por cada una de las listas que hubieran empatado y el total de los obtenidos por cada una de las demás listas, se dividirá individualmente por el cociente obtenido con anterioridad. El nuevo cociente que se obtenga dará el número de miembros con que se incrementará el obtenido por las listas mayoritarias y el que corresponda a cada una de las demás.";
-listaArt105.appendChild(cuartoElementoArt105);
-
-const quintoElementoArt105 = document.createElement("li");
-quintoElementoArt105.innerText = "Si se tratar de elección de miembros de Comisión Directiva, los cargos de presidente, vicepresidente 1º, vicepresidente 2º, secretario general, tesorero, intendente, secretario de actas, prosecretario general, protesorero y subintendente, y de los diez miembros que se desempeñarán como vocales, en ese orden y en forma alternada, serán adjudicados a las distintas listas empatadas, comenzándose por adjudicar la presidencia al socio con mayor antigüedad que encabece una de ellas.";
-listaArt105.appendChild(quintoElementoArt105);
-
-const subArt106 = document.createElement("h4");
-subArt106.innerText = "Artículo 106";
-infoSecundaria.appendChild(subArt106);
-
-const parrafoArt106 = document.createElement("p");
-parrafoArt106.innerText = "Los cargos de la Comisión Fiscalizadora se adjudicarán dividiendo el total de los votos emitidos por siete. El número de votos obtenidos por cada lista se dividirá luego por este cociente y el nuevo que resulte dará el número que corresponda a cada una, adjudicándose a los mayores residuos los que falten. la Junta Electoral procederá de inmediato a proclamar electos y hasta completar el número asignado, a los candidatos que ocupan los puestos en cada lista. Acto continuo, extenderá sus nombramientos y los convocará para que celebren sesión constitutiva dentro de los plazos establecidos. Los restantes integrantes de las listas quedarán como miembros reemplazantes de los electos por cada una, para el caso de vacancia.";
-infoSecundaria.appendChild(parrafoArt106);
 
 
 
